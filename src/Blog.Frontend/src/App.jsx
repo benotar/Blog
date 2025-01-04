e891c13afe87667ce7d0294ff33893b9966e9efb
Ivan <zhmur.ivan@gmail.com>
2025-01-04 15:07:52 +0200

    feat(frontend): create pages and routes

--- src/Blog.Frontend/src/App.jsx
+++ src/Blog.Frontend/src/App.jsx
@@ -1,5 +1,23 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import About from "./pages/About.jsx";
+import SignIn from "./pages/SignIn.jsx";
+import SignUp from "./pages/SignUp.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
+import Projects from "./pages/Projects.jsx";
+
 export default function Main() {
     return (
-        <h1 className="text-3xl text-red-950">App</h1>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/about" element={<About/>}/>
+                <Route path="/sign-in" element={<SignIn/>}/>
+                <Route path="/sign-up" element={<SignUp/>}/>
+                <Route path="/dashboard" element={<Dashboard/>}/>
+                <Route path="/dashboard" element={<Dashboard/>}/>
+                <Route path="/projects" element={<Projects/>}/>
+            </Routes>
+        </BrowserRouter>
     );
 }
