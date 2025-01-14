6f547201dcb9120ba69064e3b43669f30880e1c9
Ivan <zhmur.ivan@gmail.com>
2025-01-14 14:04:46 +0200

    feat(frontend): add sign in page

--- src/Blog.Frontend/vite.config.js
+++ src/Blog.Frontend/vite.config.js
@@ -4,14 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
     server: {
-        host: "0.0.0.0",
-        watch: {
-          usePolling: true
-        },
-        strictPort: true,
         proxy: {
             "/api": {
-                target: process.env.VITE_API_URL,
+                target: "https://blog-backend-drhpexgffqbtd8e5.polandcentral-01.azurewebsites.net/",
                 changeOrigin: true,
                 secure: false,
             }
