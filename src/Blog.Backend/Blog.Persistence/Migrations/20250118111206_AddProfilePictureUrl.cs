3b6252fc38c2520419f60d5b4e70a428b73cf923
Ivan <zhmur.ivan@gmail.com>
2025-01-18 13:13:38 +0200

    feat(backend): add profile picture url migration

---
+++ src/Blog.Backend/Blog.Persistence/Migrations/20250118111206_AddProfilePictureUrl.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Blog.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProfilePictureUrl : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ProfilePictureUrl",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProfilePictureUrl",
+                table: "Users");
+        }
+    }
+}
