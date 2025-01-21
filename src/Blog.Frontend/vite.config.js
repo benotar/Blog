2b834ffbcc7cd9b26e4d6ea07c209e20ba133488
Ivan <zhmur.ivan@gmail.com>
2025-01-21 16:43:57 +0200

    feat(frontend): add docker container support

--- src/Blog.Frontend/vite.config.js
+++ src/Blog.Frontend/vite.config.js
@@ -4,9 +4,14 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
     server: {
+        host: "0.0.0.0",
+        watch: {
+            usePolling: true
+        },
+        strictPort: true,
         proxy: {
             "/api": {
-                target: "https://blog-backend-drhpexgffqbtd8e5.polandcentral-01.azurewebsites.net/",
+                target: import.meta.env.VITE_API_URL,
                 changeOrigin: true,
                 secure: false,
             }
