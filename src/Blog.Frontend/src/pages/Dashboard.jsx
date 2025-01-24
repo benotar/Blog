620349bb9109c81b612fc8713efe876522ef5896
Ivan <zhmur.ivan@gmail.com>
2025-01-24 19:39:12 +0200

    feat(frontend): complete sidebar of the dashboard

--- src/Blog.Frontend/src/pages/Dashboard.jsx
+++ src/Blog.Frontend/src/pages/Dashboard.jsx
@@ -1,5 +1,28 @@
+import {useLocation} from "react-router-dom";
+import {useEffect, useState} from "react";
+import DashSidebar from "../components/DashSidebar.jsx";
+import DashProfile from "../components/DashProfile.jsx";
+
 export default function Dashboard() {
-    return(
-        <div>Dashboard</div>
+    const location = useLocation();
+    const [tab, setTab] = useState("");
+
+    useEffect(() => {
+        const urlParams = new URLSearchParams(location.search);
+        const tabFromUrl = urlParams.get("tab");
+        if (tabFromUrl) {
+            setTab(tabFromUrl);
+        }
+    }, [location.search]);
+
+    return (
+        <div className="min-h-screen flex flex-col md:flex-row">
+            {/* Sidebar */}
+            <div className="md:w-56">
+                <DashSidebar/>
+            </div>
+            {/* Profile... */}
+            {tab === "profile" && <DashProfile/>}
+        </div>
     );
 }
