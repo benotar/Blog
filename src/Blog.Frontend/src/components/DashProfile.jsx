43687d8d13fbab0332a582f1c1494ef78acc0f14
Ivan <zhmur.ivan@gmail.com>
2025-01-29 14:11:55 +0200

    feat(frontend): add update password in dash profile

--- src/Blog.Frontend/src/components/DashProfile.jsx
+++ src/Blog.Frontend/src/components/DashProfile.jsx
@@ -126,6 +126,7 @@ export default function DashProfile() {
         }
     }
 
+    
     return (
         <div className="max-w-lg mx-auto p-3 w-full">
             <h1 className="my-7 text-center font-semibold text-3xl">Profile</h1>
@@ -190,6 +191,18 @@ export default function DashProfile() {
                     id="email"
                     placeholder="email"
                     defaultValue={currentUser.email}
+                    onChange={handleChange}
+                />
+                <TextInput
+                    type="password"
+                    id="currentPassword"
+                    placeholder="current password"
+                    onChange={handleChange}
+                />
+                <TextInput
+                    type="password"
+                    id="newPassword"
+                    placeholder="new password"
                     onChange={handleChange}
                 />
                 <Button
