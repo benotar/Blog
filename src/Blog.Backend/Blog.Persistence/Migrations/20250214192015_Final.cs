8c16e2a49ef79a92ab061911a735c0c7bcdb4055
Ivan <zhmur.ivan@gmail.com>
2025-02-14 21:22:21 +0200

    feat(backend): add final migration

---
+++ src/Blog.Backend/Blog.Persistence/Migrations/20250214192015_Final.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Blog.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Final : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Comments_PostId",
+                table: "Comments");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_PostId_CreatedAt",
+                table: "Comments",
+                columns: new[] { "PostId", "CreatedAt" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Comments_PostId_CreatedAt",
+                table: "Comments");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_PostId",
+                table: "Comments",
+                column: "PostId");
+        }
+    }
+}
